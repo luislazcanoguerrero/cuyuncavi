2ec6f9ddb44f61fbc82b6ce7eac965421b277973
Luis Lazcano Guerrero <llazcano@dgac.gob.cl>
2025-11-21 09:27:38 -0300

    Control de Capas

--- js/app.js
+++ js/app.js
@@ -1,18 +1,14 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+
     //1. Crar el objeto mapa
     let map = L.map('map', { fullscreenControl: true, fullscreenControlOptions: { position: 'topleft' } });
 
-
-
-
-
-    //2. Estableciendo el centro y nivel de zoom
-    map.setView([-33.393850938743675, -71.12627685070039], 18)
+    // CREA OBJETO LAYER CONTROL PARA CONTROLAR LA CAPAS
+    const layerControl = L.control.layers(null,null,{ position: 'topleft',collapsed:false});
 
     //3.- Añadir una cartografía base
-
     const Topografica = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
     });
@@ -26,39 +22,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const OpenStreetMap = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap<\/a> contributors',
         errorTileUrl: '0',
-        minZoom: 3
+        minZoom: 3,
+        maxZoom: 19
     });
-
-
     OpenStreetMap.addTo(map);
 
-    var baseMaps = {
-        "Satelital": Satelital,
-        "Topográfica": Topografica,
-        "Open Street Map": OpenStreetMap
-    };
-
-   
-
-    const salon = L.marker([-33.40174220812829, -71.13112628459932]);
-    const lazcano = L.marker([-33.400117449531265, -71.1274838447571]);
-    const barrera = L.marker([-33.40544666167808, -71.14462852478029]);
-    const ampuero = L.marker([-33.39134725917198, -71.12310111522676]);
-    const mirta = L.marker([-33.39304027400479, -71.12457633018495]);
-
-
-    const puntos = L.layerGroup([salon, lazcano, barrera, ampuero, mirta]);
-
-
-    var overlayMaps = {
-        "P.Encuentro ": puntos
-    };
-
-    var layerControl = L.control.layers(baseMaps,overlayMaps,{ position: 'topleft' }).addTo(map);
-
-
-
-    // 4. Añadiendo un visor con las coordenadas
+    // AÑADIENDO UN RECUEDRO PARA LAS COORDENADAS
     L.control.coordinates({
         position: "bottomleft",
         decimals: 6,
@@ -69,11 +38,26 @@ document.addEventListener('DOMContentLoaded', () => {
     }).addTo(map);
 
 
-    //5. Insertando una leyenda en el mapa
+    // MARCARDO INICIALMENTE UN CENTRO DEL MAPA
+    map.setView([-33.393850938743675, -71.12627685070039], 18)
+
+
+    // CREANDO MARCADORES PARA LUGARES DE REUNIÓN
+    const salon = L.marker([-33.40174220812829, -71.13112628459932]);
+    const lazcano = L.marker([-33.398629676103276,-71.12742483615877]);
+    const barrera = L.marker([-33.40544666167808, -71.14462852478029]);
+    const ampuero = L.marker([-33.39134725917198, -71.12310111522676]);
+    const mirta = L.marker([-33.39304027400479, -71.12457633018495]);
+
+    // AGRUPARLOS EN UN LAYERGROUP
+    const puntos = L.layerGroup([salon, lazcano, barrera, ampuero, mirta]);
+
+        
+
+    // Insertando una imagen en el mapa en la esquina superior derecha
     var legend = L.control({
         position: 'topright'
     });
-
     legend.onAdd = function () {
         var div = L.DomUtil.create('div', 'legend');
         div.innerHTML += '<img alt="Descripcion de la imagen" src="/images/predi.png" width="80px" title="Estas buenas nuevas del Reino se predicarán en toda la tierra habitad">';
@@ -82,14 +66,13 @@ document.addEventListener('DOMContentLoaded', () => {
     legend.addTo(map);
 
     //add geojson layer
-    const geoJsonUrl = 'data/territorios.json'
-
     // aqui cargo el geojson pero no parece ocurrir nada
     // Vario la opacidad por cada feature
+    const geoJsonUrl = 'data/territorios.json'
     fetch(geoJsonUrl)
         .then(response => response.json())
         .then(data => {
-            L.geoJSON(data, {
+            const capaLimites = L.geoJSON(data, {
                 style: function (feature) {
                     const { codregion, fillColor } = feature.properties
                     return {
@@ -101,14 +84,17 @@ document.addEventListener('DOMContentLoaded', () => {
                     };
                 },
                 onEachFeature: muestraFeatureDatos
-            }).addTo(map)
+            })
+            capaLimites.addTo(map)
+            layerControl.addOverlay(capaLimites, 'Limites');
         })
         .catch(function (error) {
             console.log(`Se produjo el error ->  ${error}`)
         })
 
+
     const botonera = document.querySelector("#botonera")
-    
+
     // Botonera de acceso direct
     botones.forEach(rotulo => {
         const { name, latitud, longitud } = rotulo
@@ -117,31 +103,22 @@ document.addEventListener('DOMContentLoaded', () => {
         boton.innerHTML = `${name.toString().padStart(3, '0')}`
 
         boton.addEventListener('click', (e) => {
-            map.flyTo([latitud, longitud], 18)
+            map.flyTo([latitud, longitud], 19)
         })
         botonera.appendChild(boton)
     })
 
-    // Rotulos de los territorios
+    // Capa de rotulos de los territorios
+    numeros = L.layerGroup();
     rotulos.forEach(rotulo => {
         const { name, latitud, longitud } = rotulo
-
-        L.circleMarker([latitud, longitud], {
-            radius: 20,
-            fillColor: '#ffff00',
-            color: '#000000',
-            fillOpacity: 1
-        }).addTo(map);
-
-        var text = L.tooltip({
+        numeros.addLayer(L.tooltip({
             permanent: true,
             direction: 'center',
             className: 'my-label'
-        })
-            .setContent(name)
-            .setLatLng([latitud, longitud]);
-        text.addTo(map)
+        }).setContent(name).setLatLng([latitud, longitud]))
     })
+    numeros.addTo(map)
 
 
     function muestraFeatureDatos(feature, layer) {
@@ -158,6 +135,25 @@ document.addEventListener('DOMContentLoaded', () => {
             layer.bindPopup(`Territorio N°: ${feature.properties.name}`);
         }
     }
+
+
+    // AGREGANDO UN LAYER CONTROL PARA LOS DIFERENTES LAYES DEL MAPA
+    layerControl.addBaseLayer(Satelital, "Satelital");
+    layerControl.addBaseLayer(Topografica, "Satelital");
+    layerControl.addBaseLayer(OpenStreetMap, "Open Street Map");
+    layerControl.addOverlay(puntos, "P. Encuentro")
+    layerControl.addOverlay(numeros, 'Número Territorios');
+    layerControl.addTo(map)
+    
+    
+    // Oculta o Muestra capa dependiendo del nivel de zoom
+    map.on('zoomend', function (e) {
+        if (map.getZoom() < 16){
+            map.removeLayer(numeros);
+        }else if (map.getZoom() >= 16){
+            map.addLayer(numeros);
+        }
+    })
 
 
 })
